773832d3149f7d898860ef3f0848f7fd15bd34e2
unknown <khachatryanero.2002@mail.ru>
2023-02-04 12:58:49 +0400

    Updated element click

--- webpack/src/List/List.js
+++ webpack/src/List/List.js
@@ -1,15 +1,27 @@
-import './styles.css';
+import "./styles.css";
+
 export class List {
-  render(...items){
-    let parent = document.createElement('ul');
-    items.forEach(elm => {
-      let child = document.createElement('li');
-      child.addEventListener('click', function () {
-        this.classList.toggle("active");
-      });
+  render(...items) {
+    let parent = document.createElement("ul");
+    items.forEach((elm) => {
+      let child = document.createElement("li");
       child.innerText = elm;
       parent.append(child);
+      document.body.append(parent);
     });
-    document.body.append(parent);
+
+    const parentElm = document.querySelector("ul");
+    const childElm = document.querySelectorAll("li");
+
+    parentElm.addEventListener("click", function (evt) {
+      if (evt.target.closest("li") && evt.target.classList.contains("active")) {
+        evt.target.classList.remove("active");
+      } else if (evt.target.closest("li")) {
+        childElm.forEach((elm) => {
+          elm.classList.remove("active");
+        });
+        evt.target.classList.toggle("active");
+      }
+    });
   }
 }
